b292fb21be4955606b3d4d54095c215829b17a88
SanaGo108 <oksana135@mail.ru>
2024-06-26 22:57:55 +0300

    new

--- lights/lights/spiders/lightsnew.py
+++ lights/lights/spiders/lightsnew.py
@@ -1,12 +1,15 @@
 import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
 
 class LightsnewSpider(scrapy.Spider):
     name = "lightsnew"
-    allowed_domains = ["https://divan.ru"]
+    allowed_domains = ["divan.ru"]
     start_urls = ["https://www.divan.ru/category/podvesnye-svetilniki"]
 
     def parse(self, response):
-        lights = response.css('div._Ud0k')
+        lights = response.css('_Ud0k U4KZV')
         for light in lights:
             yield {
                 'name' : light.css('div.lsooF span::name').get(),
